6769ce6a0586a29884174faa3257d492168e122e
phipnye <75276332+phipnye@users.noreply.github.com>
2022-03-04 13:35:46 -0800

    Create Exercise 1.3.1.c

---
+++ Level 1/Exercise 1.3.1.c
@@ -0,0 +1,12 @@
+/*This is my first exercise in C - program.This exercise shows a simple print function*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("My first C-program\nis a fact!\nGood, isn't it?");
+	/*Prints the above statement on three different lines*/
+
+	return 0;
+
+}
